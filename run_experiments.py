1c15fab5b01668c891fd64ee35a1040c31f5223a
Andrew Wong <awong@cloudera.com>
2018-05-10 13:53:04 -0700

    flesh out connections
    We can now connect to servers.
    Also adds a way for reading secrets.

--- run_experiments.py
+++ run_experiments.py
@@ -30,66 +30,67 @@ import urllib2
 import yaml
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-PASSWORD = "awong"
 
-class Cluster:
-    class Server(object):
-        def __init__(self, config):
-            self.config = config
-            self.addresses = self.config['addresses']
-            self.setup = None
-            if len(self.config['setup_script']) > 0:
-                self.setup = os.path.abspath(self.config['setup_script'])
-            self.flags = []
-            self.dirs = []
-            for name, path in self.config['dir_flags'].iteritems():
-                self.flags.append("--{}={}".format(name, path))
-                self.dirs.extend(path.split(','))
+class Server(object):
+    def __init__(self, config, secrets):
+        self.config = config
+        self.secrets = secrets
+        self.addresses = self.config['addresses']
+        self.setup_script = None
+        if len(self.config['setup_script']) > 0:
+            self.setup_script = os.path.abspath(self.config['setup_script'])
+        self.flags = []
+        self.dirs = []
+        for name, path in self.config['dir_flags'].iteritems():
+            self.flags.append("--{}={}".format(name, path))
+            self.dirs.extend(path.split(','))
+        logging.info("flags: {}, dirs: {}".format(self.flags, self.dirs))
+        logging.info("addrs: {}".format(self.addresses))
 
-        # Create the directories and run any setup.
-        def setup(self):
+    # Create the directories and run any setup.
+    def setup(self):
+        self.clients = {}
+        for addr in self.addresses:
+            logging.info("Connecting to server {}".format(addr))
+
             # Create client.
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            key = paramiko.RSAKey.from_private_key_file("")
-            client.connect(hostname="", username="", pkey=key)
+            if addr in self.secrets.keys():
+                s = self.secrets[addr]
+                logging.info("Reading secrets for {}".format(addr))
+                key = paramiko.RSAKey.from_private_key_file(s['private_key_file'], password=s['password'])
+                client.connect(hostname=addr, username=s['user'], pkey=key)
+            else:
+                logging.info("No secrets listed for server")
+                client.connect(hostname=addr)
+            logging.info("Connected to server {}".format(addr))
+            stdin, stdout, stderr = client.exec_command("mkdir -p {}".format(" ".join(self.dirs)))
+            self.clients[addr] = client
 
-            # Check if there exists the binary already.
-            # TODO: send over the setup script and run setup.
-            return
+        # Check if there exists the binary already.
+        # TODO: send over the setup script and run setup.
 
-    def __init__(self, config):
+    def cleanup(self):
+        for addr in self.addresses:
+            client = self.clients[addr]
+            stdin, stdout, stderr = client.exec_command("rm -rf {}".format(" ".join(self.dirs)))
+            for line in stdout:
+                logging.info(line)
+
+class Cluster:
+    def __init__(self, base_opts, config, secrets):
         self.config = config
-        self.cred_file = None
-        if len(self.config['cred_file']) > 0:
-            self.cred_file = os.path.abspath(self.config['cred_file'])
-        self.masters = Server(self.config['masters'])
-        self.tservers = Server(self.config['tservers'])
+        self.masters = Server(self.config['masters'], secrets)
+        self.masters.setup()
+        self.tservers = Server(self.config['tservers'], secrets)
+        self.tservers.setup()
+        logging.info("cleaning up")
+        self.masters.cleanup()
+        self.tservers.cleanup()
 
     def start_servers():
         return
-
-    @property
-    def results_dir(self):
-        path = os.path.join(BASE_DIR, "results")
-        for dim_name, dim_val in sorted(self.dimensions.iteritems()):
-            path = os.path.join(path, "%s=%s" % (dim_name, dim_val))
-        return path
-    
-    @property
-    def log_dir(self):
-        return os.path.join(self.results_dir, "logs")
-
-    def flags(self, config_key):
-        ret = []
-        for flag, value in self.config[config_key].iteritems():
-        if type(value) == list:
-            value = ",".join(value)
-        ret.append("--%s=%s" % (flag, value))
-        return ret
-
-    def workload_path(self, workload):
-        return os.path.join(self.workload_dir, workload)
 
 def start_servers(config):
     logging.info("Starting servers...")
@@ -149,7 +150,7 @@ def dump_ts_info(exp, suffix):
                 ("metrics?include_raw_histograms=1", "metrics"),
                 ("mem-trackers?raw", "mem-trackers")]:
         fname = "%s-%s.txt" % (fname, suffix)
-        dst = file(os.path.join(exp.results_dir, fname), "w")
+        dst = file("")
         try:
             shutil.copyfileobj(urllib2.urlopen("http://localhost:8050/" + page), dst)
         except:
@@ -158,9 +159,6 @@ def dump_ts_info(exp, suffix):
             sys.exit(1)
 
 def run_experiment(exp):
-    if os.path.exists(exp.results_dir):
-        logging.info("Skipping experiment %s (results dir already exists)" % exp.dimensions)
-        return
     logging.info("Running experiment %s" % exp.dimensions)
     stop_servers()
     remove_data()
@@ -187,17 +185,18 @@ def generate_dimension_combinations(setup_yaml):
         combos = new_combos
     return combos
 
-def load_clusters(setup_yaml):
+def load_clusters(setup_yaml, secrets_yaml):
     base_opts = setup_yaml['base_opts']
     clusters = {}
     for name, config in setup_yaml['clusters'].iteritems():
-        clusters[name] = Cluster(base_opts, config)
+        logging.info("Connecting to cluster: {}".format(name))
+        clusters[name] = Cluster(base_opts, config, secrets_yaml)
     return clusters, base_opts
 
 
 def run_all(clusters, opts):
-    for exp in exps:
-        run_experiment(exp)
+    for c in clusters:
+        run_experiment(c)
 
 
 def main():
@@ -207,10 +206,16 @@ def main():
         type=str,
         help="YAML file describing experiments to run",
         default=os.path.join(BASE_DIR, "setup.yaml"))
+    p.add_argument("--secrets-yaml",
+        dest="secrets_yaml_path",
+        type=str,
+        help="YAML file containing user info",
+        default=os.path.join(BASE_DIR, "secrets.yaml"))
     args = p.parse_args()
     setup_yaml = yaml.load(file(args.setup_yaml_path))
-    clusters, opts = load_clusters(setup_yaml)
-    run_all(clusters, opts)
+    secrets_yaml = yaml.load(file(args.secrets_yaml_path))
+    clusters, opts = load_clusters(setup_yaml, secrets_yaml)
+    # run_all(clusters, opts)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
